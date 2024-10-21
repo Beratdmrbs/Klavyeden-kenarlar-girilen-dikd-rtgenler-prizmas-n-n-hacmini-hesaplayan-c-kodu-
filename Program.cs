fc84dfa879e1e210a0d06d225609ed5207537eaa
Beratdmrbs <beratdmrbs61@gmail.com>
2024-10-21 22:07:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Birinci kenar uzunluğunu girin (a): ");
+
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("İkinci kenar uzunluğunu girin (b): ");
+
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Üçüncü kenar uzunluğunu girin (c): ");
+
+double c = Convert.ToDouble(Console.ReadLine());
+
+double hacim = a * b * c;
+
+Console.WriteLine("Dikdörtgenler prizmasının hacmi: {hacim}");
